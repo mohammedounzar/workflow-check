71e57c0b95a2352513162578dab1171185379908
mohammedounzar <ounzarmohammed@gmail.com>
2025-07-02 12:26:35 +0100

    Pass github_token to update_sheet function for enhanced security

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     error_msg = os.getenv("ERROR_MSG")
     pr_number = int(os.getenv("PR_NUMBER"))
     github_token = os.getenv("GITHUB_TOKEN")
-    
+
     print(f"Sheet ID: {sheet_id}, Status: {status}, PR Number: {pr_number}, Error Message: {error_msg}")
 
     if not all([sheet_id, status, pr_number]):
         raise ValueError("Missing one or more required environment variables.")
 
-    update_sheet(sheet_id, pr_number, status, error_msg)
+    update_sheet(sheet_id, pr_number, status, error_msg, github_token)
 
 if __name__ == "__main__":
     try:
