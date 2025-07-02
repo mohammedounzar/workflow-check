320d1feb64c8b82fd845efe4c05b142c8fef8493
mohammedounzar <ounzarmohammed@gmail.com>
2025-07-02 10:11:45 +0100

    Add regex functionality to extract ticket numbers and sheet names from commit messages

--- extract_ticket_and_sheet.py
+++ extract_ticket_and_sheet.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+import re
 
 def extract_ticket_and_sheet(pr_number):
     load_dotenv()
@@ -20,5 +21,20 @@ def extract_ticket_and_sheet(pr_number):
     else:
         print(f"Error {response.status_code}: {response.text}")
 
-    commit_message = data[0]['items']['commit']['message']['examples'] 
-    print(f"Commit message: {commit_message}")
+    commit_msg = data[-1]['commit']['msg']
+
+    print(f"Commit msg: {commit_msg}")
+
+    ticket_nbr = re.findall(r'\bEEV2-\d+\b', commit_msg)
+    if ticket_nbr:
+        print(f"Ticket number found: {ticket_nbr[0]}")
+    else:
+        raise ValueError("No valid ticket number found in commit message.")
+    
+    sheet_name = re.findall(r'\brelease-\d+\.\d+\.\d+\b', commit_msg)
+    if sheet_name:  
+        print(f"Sheet name found: {sheet_name[0]}")
+    else:
+        raise ValueError("No valid sheet name found in commit message.")
+    
+    return ticket_nbr[0], sheet_name[0]
