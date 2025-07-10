9b008aed2344537342374ca67720d8def5b0c3ba
mohammedounzar <ounzarmohammed@gmail.com>
2025-07-10 20:18:01 +0100

    Update test case for bugfix EV2-2022 to clarify regex support

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ from rebuild_ticket_nbr_test import rebuild_ticket_nbr
     ("fix E2 1234 and EEV2 5678", "EEV2-1234"),
     ("this fixes EV2 3210 as discussed", "EEV2-3210"),
     ("patch\nEE2 1111", "EEV2-1111"),
-    ("bugfix    EV2-2022", "EEV2-2022"),  # if regex supports it
+    ("bugfix    EV2-2022", "EEV2-2022"),
     ("fix applied to EEV2 9876", "EEV2-9876"),
 ])
 def test_rebuild_ticket_nbr(input_str, expected):
